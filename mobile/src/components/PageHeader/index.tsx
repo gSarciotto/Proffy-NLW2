8ffc3eb3dcfa2e72bea1deb84b096a7755c94041
Code <gsarciotto@gmail.com>
2020-08-07 10:34:19 -0300

    mobile done

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,43 @@
+import React, { ReactNode } from "react";
+import { View, Image, Text } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+
+import backIcon from "../../assets/images/icons/back.png";
+import logoImg from "../../assets/images/logo.png";
+
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+interface PageHeaderProps {
+    title: string;
+    headerRight?: ReactNode;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.navigate("Landing");
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.topBar}>
+                <BorderlessButton onPress={handleGoBack}>
+                    <Image source={backIcon} resizeMode="contain" />
+                </BorderlessButton>
+
+                <Image source={logoImg} resizeMode="contain" />
+            </View>
+            
+            <View style={styles.header}>
+                <Text style={styles.title}>{props.title}</Text>
+                {props.headerRight}
+            </View>
+
+            {props.children}
+        </View>
+    );
+}
+
+export default PageHeader;
